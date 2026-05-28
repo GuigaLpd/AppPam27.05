a6979435e50e297449d8c742326dc01f1bf8b97c
GuigaLpd <guilhermecampos0108@gmail.com>
2026-05-27 22:30:45 -0300

    .

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,51 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
-
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TelaPrincipal()
-        }
-    }
-}
-@Composable
-fun TelaPrincipal() {
-    MyApplicationTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding).background(Color.Red)) {
-                Text("aasd")
-                Text("aasd")
-                Text("aasd")
-                Text("aasd")
+            Column(
+                modifier = Modifier.background(color = Color.Red).fillMaxSize(),
+                Arrangement.Center,
+                Alignment.End
+            ) {
+                var vlrDoTextField by remember { mutableStateOf("vlrIni")}
+                Text("asd")
+                Text("asd")
+                TextField(vlrDoTextField, onValueChange = {
+                    vlrDoTextField = it
+            })
+                OutlinedTextField(value= vlrDoTextField, onValueChange = {
+                    Log.i("####", it)
+                    vlrDoTextField = it
+                })
             }
         }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }

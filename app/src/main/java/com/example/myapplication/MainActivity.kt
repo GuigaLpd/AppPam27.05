180170c769c8c4a3298025ea152684f7f46022d0
wagnermarques <wagnerdocri@gmail.com>
2026-06-10 22:30:59 -0300

    Atualizando atividade.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,22 +9,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,20 +27,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier.background(color = Color.Red).fillMaxSize(),
-                Arrangement.Center,
-                Alignment.End
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End
             ) {
-                var vlrDoTextField by remember { mutableStateOf("vlrIni")}
+                var vlrDoTextField by remember { mutableStateOf("vlrIni") }
+
                 Text("Ada Lovelace")
                 Text("Grace Hopper")
-                Text("Charles Babbage")
-                TextField(vlrDoTextField, onValueChange = {
-                    vlrDoTextField = it
-            })
-                OutlinedTextField(value= vlrDoTextField, onValueChange = {
-                    Log.i("####", it)
-                    vlrDoTextField = it
-                })
+
+                TextField(
+                    value = vlrDoTextField,
+                    onValueChange = { vlrDoTextField = it }
+                )
+
+                OutlinedTextField(
+                    value = vlrDoTextField,
+                    onValueChange = {
+                        Log.i("####", it)
+                        vlrDoTextField = it
+                    }
+                )
             }
         }
     }
